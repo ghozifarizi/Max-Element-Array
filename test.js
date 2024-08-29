cf23bf1159fd1a6b09d36945fbe6bca8df0a763e
unknown <ghozifarizi11@gmail.com>
2024-08-29 17:25:01 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+
+const findMax = require('max-element');
+
+
+// contoh 
+const bilangan = [3, 5, 7, 2, 8, 100];
+const maks = findMax(bilangan);
+
+console.log(`Nilai Terbesarnya adalah: ${maks}`);  
